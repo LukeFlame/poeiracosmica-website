e08284756891b87544178d4d2ace2e100950900a
LukeFl_ <69337083+LukeFlame@users.noreply.github.com>
2021-07-16 23:57:56 -0300

    Add adsense

--- pages/_app.js
+++ pages/_app.js
@@ -143,6 +143,7 @@ export default function App({ Component, pageProps }) {
         <meta name="title" content="Poeira Cósmica | Servidor de Discord" />
         <meta name="description" content="Poeira Cósmica é um servidor de Discord com uma comunidade diversa que preza pela ciência, filosofia... e humor." />
 
+        <script data-ad-client="ca-pub-5541508909226142" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
         <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png" />
         <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png" />
         <link rel="apple-touch-icon" sizes="72x72" href="/favicon/pple-icon-72x72.png" />
