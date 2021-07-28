1f5ed5b2bc8dde428df637a973ccdb41d35557e4
LukeFl_ <69337083+LukeFlame@users.noreply.github.com>
2021-07-28 10:18:14 -0300

    update staff team

--- pages/sobre.js
+++ pages/sobre.js
@@ -297,6 +297,24 @@ function About({ dataStaff, dataSearch }) {
                           </div>
                         </a>
                       </Link>
+                      <Link href={`/user/${dataStaff[5].userID}`}>
+                        <a className="card">
+                          <img src={dataStaff[5].avatar} alt="" className="avatar" />
+                          <div className="texts">
+                            <h2 className="nick">{formatNick(dataStaff[5].nickname, 8)}</h2>
+                            <h3 className="info">Moderadora desde 2020</h3>
+                          </div>
+                        </a>
+                      </Link>
+                      <Link href={`/user/${dataStaff[6].userID}`}>
+                        <a className="card">
+                          <img src={dataStaff[6].avatar} alt="" className="avatar" />
+                          <div className="texts">
+                            <h2 className="nick">{formatNick(dataStaff[6].nickname, 8)}</h2>
+                            <h3 className="info">Trial desde 2021</h3>
+                          </div>
+                        </a>
+                      </Link>
                     </div>
                   </div>
                 )}
@@ -314,7 +332,7 @@ function About({ dataStaff, dataSearch }) {
 // Calls database data to the staff info in staff option and search bar
 // Totally static and updated every 15s
 export async function getStaticProps() { // hugo luke calabi davi kipper
-  const res = await fetch('https://poeiracosmica.com/api/users/345624603256684555,258041633789050881,600781695255576586,811791678523703297,521842730171629569');
+  const res = await fetch('https://poeiracosmica.com/api/users/345624603256684555,258041633789050881,600781695255576586,811791678523703297,521842730171629569,455049154452914177,431927033224757278');
   const { data } = await res.json();
 
   const resSearch = await fetch(`https://poeiracosmica.com/api/search`);
